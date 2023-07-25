d950f9da03b7b76f07f126f68372dd9d69ae22a1
CodingManoj <mvp2612@hotmail.com>
2023-07-24 21:29:36 -0400

    AutoPush

--- route-table.tf
+++ route-table.tf
@@ -12,6 +12,13 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+# Associates the route-table to the public-subnet
+resource "aws_route_table_association" "public_subnet_rt_association" {
+  count                 = length(aws_subnet.public_subnet.*.id)
+
+  subnet_id             = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id        = aws_route_table.public_rt.id
+}
 
 # Creates Private Route Table
 # resource "aws_route_table" "private_rt" {

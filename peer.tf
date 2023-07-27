da9ae35815859adfc8d26f01f14738eec7b252c5
CodingManoj <mvp2612@hotmail.com>
2023-07-26 20:59:55 -0400

    AutoPush

--- peer.tf
+++ peer.tf
@@ -1,8 +1,8 @@
 # Accepter's side of the connection.
 resource "aws_vpc_peering_connection_accepter" "peer" {
-  vpc_id        = aws_vpc.main.id
-  peer_vpc_id   = "vpc-0031cc952da0c7bfc"
-  auto_accept   = true
+  vpc_id                        = aws_vpc.main.id
+  vpc_peering_connection_id     = "vpc-0031cc952da0c7bfc"
+  auto_accept                   = true
 
   tags = {
     Side = "Robot-${var.ENV}-default-vpc-peering"

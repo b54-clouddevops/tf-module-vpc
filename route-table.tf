37e404ccb98e01fb973344bdc1dceb6341fb84c4
CodingManoj <mvp2612@hotmail.com>
2023-07-26 21:24:52 -0400

    AutoPush

--- route-table.tf
+++ route-table.tf
@@ -1,10 +1,15 @@
 # Creates Public Route Table
 resource "aws_route_table" "public_rt" {
-  vpc_id                = aws_vpc.main.id
+  vpc_id                      = aws_vpc.main.id
 
   route {
-    cidr_block          = "0.0.0.0/0"
-    gateway_id          = aws_internet_gateway.igw.id
+    cidr_block                = "0.0.0.0/0"
+    gateway_id                = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block                = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
 
   tags = {
